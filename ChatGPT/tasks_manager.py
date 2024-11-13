07cb1b59dad652259be42b6deea622ab23f3eb63
Chorelin <miheevila6@gmail.com>
2024-11-13 19:10:53 +0300

    tasks_manager
    tasks_manager.py - Код, который создает задачи в файле с возможностью очищения
    FIle_data.txt - Удалён из-за отсутствия большой необходимости

---
+++ ChatGPT/tasks_manager.py
@@ -0,0 +1,31 @@
+'''
+Задача: написать код, который будет создавать и удалять задачи в списке задач.
+Усложнение: запись должна производиться в файл.
+'''
+
+
+import os
+if os.path.exists('Tasks_manager_file.txt'):
+    file = open('Tasks_manager_file.txt', 'a')
+else:
+    file = open('Tasks_manager_file.txt', 'x')
+
+
+def add_task(task):
+    file.write(f'\n{task}\n')
+    file.close
+def remove_tasks():
+    i = input('Вы действительно хотите очистить спиок задач? y (Yes/Да) или n (No/Нет)\nВвод: ')
+    if i == 'y':
+        file = open('Tasks_manager_file.txt', 'w')
+        file.write(' ')
+        file.close()
+        print('Задачи успешно удалены!')
+    elif i == 'n':
+        print('Отмена удаления списка задач')
+    else:
+        print('Некорректный ввод. Выберите y(Yes/Да) или n(No/Нет)')
+# Examples
+
+# add_task('Название задачи') / Добавление задачи
+# remove_tasks() / Удаление задач
